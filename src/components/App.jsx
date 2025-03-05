4eb1e79f511abcdc2242c46dfb38a7607a3e48cf
Sviatoslav <sviat.danko@gmail.com>
2025-03-05 22:32:57 +0200

    conect jason

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import Profile from '../components/Profile/Profile.jsx';
+import userData from '../userData.json';
 
 // const [count, setCount] = useState(0);
 
@@ -8,8 +9,13 @@ export default function App() {
   return (
     <div>
       <h1>Products</h1>
-      <Profile />
-      <Profile />
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
     </div>
   );
 }

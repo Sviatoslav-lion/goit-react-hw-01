27f2494f127290150281856ad905daf56cf8e767
Sviatoslav <sviat.danko@gmail.com>
2025-03-06 22:26:56 +0200

    exercise 1

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,33 +1,33 @@
-import './Profile.module.css';
+import css from './Profile.module.css';
 
-export default function Product({
-  name, // ім'я користувача
-  tag, // тег в соціальній мережі без **`@`**
-  location, // місто і країна
-  image, // посилання на зображення
-  stats, // об'єкт з інформацією про активності
+export default function Profile({
+  name = 'Unknown', // ім'я користувача
+  tag = 'unknown', // тег в соціальній мережі без **`@`**
+  location = 'Unknown location', // місто і країна
+  image = 'https://cdn-icons-png.flaticon.com/512/2922/2922506.png', // посилання на зображення
+  stats: { followers = 0, views = 0, likes = 0 }, // об'єкт з інформацією про активності
 }) {
   return (
-    <div>
-      <div>
-        <img src={image} alt={name} />
-        <p>{name}</p>
-        <p>@{tag}</p>
-        <p>{location}</p>
+    <div className={css.profile}>
+      <div className={css.info}>
+        <img className={css.avatar} src={image} alt={name} />
+        <p className={css.name}>{name}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
       </div>
 
-      <ul>
-        <li>
-          <span>Followers</span>
-          <span>{stats.followers}</span>
+      <ul className={css.stats}>
+        <li className={css.item}>
+          <span className={css.itemInfo}>Followers</span>
+          <span className={css.itemNumbers}>{followers}</span>
         </li>
-        <li>
-          <span>Views</span>
-          <span>{stats.views}</span>
+        <li className={css.item}>
+          <span className={css.itemInfo}>Views</span>
+          <span className={css.itemNumbers}>{views}</span>
         </li>
-        <li>
-          <span>Likes</span>
-          <span>{stats.likes}</span>
+        <li className={css.item}>
+          <span className={css.itemInfo}>Likes</span>
+          <span className={css.itemNumbers}>{likes}</span>
         </li>
       </ul>
     </div>
